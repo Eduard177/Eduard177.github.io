da22167638a042b0d4fd6c3b876fe8860d7c921b
Smerlyn García <smerlyn_alejandro@outlook.com>
2022-05-25 17:21:21 -0400

    Comenzando a crear el modo oscuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,54 @@
 /*==================== SHOW MENU ====================*/
 
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+const sections = document.querySelectorAll('section[id]');
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            
+        }
+    })
+}
 
 /*==================== SHOW SCROLL TOP ====================*/
 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/
 
